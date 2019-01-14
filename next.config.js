f5da96599095aa6b8d80330ab5b7ff7f2e2ae6fb
Lars Kappert <lars@webpro.nl>
2019-01-14 16:57:30 +0100

    Migrate to next@canary (serverless)

--- next.config.js
+++ next.config.js
@@ -1,30 +1,20 @@
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {}
-    : !process.env.NOW_REGION
-    ? require('next/constants')
-    : require('next-server/constants');
+const DotEnv = require('dotenv-webpack');
+const withSass = require('@zeit/next-sass');
 
-module.exports = phase => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return {};
+const config = {
+  target: 'serverless',
+  sassLoaderOptions: {
+    includePaths: ['node_modules']
+  },
+  webpack: config => {
+    config.plugins.push(new DotEnv());
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    });
+    return config;
   }
+}
 
-  const DotEnv = require('dotenv-webpack');
-  const withSass = require('@zeit/next-sass');
-
-  return withSass({
-    sassLoaderOptions: {
-      includePaths: ['node_modules']
-    },
-    webpack: config => {
-      config.plugins.push(new DotEnv());
-      config.module.rules.push({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto'
-      });
-      return config;
-    }
-  });
-};
+module.exports = withSass(config);
